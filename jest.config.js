4f97d6a594d66a0be322e7496579552363b767c1
kirann <kirann@blackduck.com>
2025-09-14 21:15:20 +0530

    SIGINT-2368:  Action: Integrate Bridge Package Manager for Air Gap

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,14 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest'
   },
   verbose: true,
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', 'src'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json'
+    }
+  },
+  testEnvironment: 'node',
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 }
