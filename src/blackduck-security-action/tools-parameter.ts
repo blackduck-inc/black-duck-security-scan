9e2e199be19ea1bf4a77f22f380fefb84cc95950
renukac <renukac@blackduck.com>
2026-02-17 11:48:29 +0530

    feat(SIGINT-4607): Removed Info log

--- src/blackduck-security-action/tools-parameter.ts
+++ src/blackduck-security-action/tools-parameter.ts
@@ -322,7 +322,6 @@ export class BridgeToolsParameter {
     fs.writeFileSync(stateFilePath, inputJson)
 
     debug('Generated state json file at - '.concat(stateFilePath))
-    info('Polaris Input.json content - '.concat(inputJson))
 
     command = BridgeToolsParameter.STAGE_OPTION.concat(BridgeToolsParameter.SPACE).concat(BridgeToolsParameter.POLARIS_STAGE).concat(BridgeToolsParameter.SPACE).concat(BridgeToolsParameter.INPUT_OPTION).concat(BridgeToolsParameter.SPACE).concat(stateFilePath).concat(BridgeToolsParameter.SPACE)
     return command
