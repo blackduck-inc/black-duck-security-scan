35dff3c1cf990e56a3bb3c3f56064930ba4ca356
renukac <renukac@blackduck.com>
2026-02-27 13:13:06 +0530

    Updated review comment

--- src/blackduck-security-action/tools-parameter.ts
+++ src/blackduck-security-action/tools-parameter.ts
@@ -764,9 +764,13 @@ export class BridgeToolsParameter {
   private getGithubBranchName(): string {
     let branchName = ''
     const inputPrCommentSeverities = inputs.POLARIS_PRCOMMENT_SEVERITIES
-    if (parseToBoolean(inputs.POLARIS_PRCOMMENT_ENABLED) && inputPrCommentSeverities != null && inputPrCommentSeverities.length > 0) {
+    if (parseToBoolean(inputs.POLARIS_PRCOMMENT_ENABLED)) {
+      if (inputPrCommentSeverities != null && inputPrCommentSeverities.length > 0) {
+        // Only polaris use case
+        branchName = process.env[constants.GITHUB_ENVIRONMENT_VARIABLES.GITHUB_HEAD_REF] || ''
+      }
       // Only polaris use case
-      branchName = process.env[constants.GITHUB_ENVIRONMENT_VARIABLES.GITHUB_HEAD_REF] || ''
+      branchName = process.env[constants.GITHUB_ENVIRONMENT_VARIABLES.GITHUB_REF_NAME] || ''
     } else {
       // For pull requests, non-pull requests and manual trigger events
       if (process.env[constants.GITHUB_ENVIRONMENT_VARIABLES.GITHUB_HEAD_REF] !== '') {
