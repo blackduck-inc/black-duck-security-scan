28df2f5599433a5a42fce04fed0b1144fef6f922
lokeshbd <lokesha@blackduck.com>
2026-04-02 17:21:26 +0530

    fix: SIGINT-4889 - vuln fixes

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,12 @@ module.exports = {
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/unit/**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': ['ts-jest', { useESM: false }]
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(.*@octokit/(request-error|plugin-paginate-rest))/)'
+  ],
   verbose: true,
   "moduleDirectories": [
     "node_modules",
